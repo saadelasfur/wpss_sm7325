dddcddf61afbd9a82ad2b6a55541036d96874e9d
Saad <saadelasfur@gmail.com>
2023-01-11 15:50:14 +0300

    Initial commit
    Co-authored-by: Simon1511 <me@simon1511.de>
    Co-authored-by: ShaDisNX255 <rayo_dis_nx@hotmail.com>

---
+++ README.md
@@ -0,0 +1,4 @@
+# A simple recovery flashable ZIP to add bootloader support for Galaxy M52 5G
+
+## Downloads
+- The ZIP file can be downloaded from the [releases](https://github.com/saadelasfur/wpss_m52xq/tree/main/releases) directory.
---
+++ make_zip.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# Copyright (C) 2023 saadelasfur
+
+echo ""
+echo "Making zip..."
+
+[ -d "out" ] && rm -rf out
+mkdir out
+
+SRC=$(pwd)
+OUT_DIR=$SRC/out
+INSTALLER_ZIP="$SRC/Installer/Installer.zip"
+
+read -p "version: " VERSION
+for dir in SM-M526B SM-M526BR; do
+    cp -a --preserve=all "$SRC/$dir" "$OUT_DIR/"
+done
+7z x "$INSTALLER_ZIP" -o"$OUT_DIR/"
+cat "$SRC/Installer/updater-script" > "$OUT_DIR/META-INF/com/google/android/updater-script"
+cd "$OUT_DIR"
+find . -exec touch -a -c -m -t 200812311700.00 {} +
+7z a -tzip -mx=5 "WiFi Fix - v$VERSION (m52xq).zip" .
+mv "WiFi Fix - v$VERSION (m52xq).zip" "$SRC/releases/"
+cd $SRC
+
+sleep 1 && exit 0
