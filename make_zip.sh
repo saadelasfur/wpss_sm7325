d446ad4916b241fb7a48a0bb97c96d06ca58c0ac
Saad <saadelasfur@gmail.com>
2024-12-14 23:06:26 +0300

    Automate the updating process

--- make_zip.sh
+++ make_zip.sh
@@ -20,6 +20,7 @@ done
 7z x "$INSTALLER_ZIP" -o"$OUT_DIR/"
 cat "$SRC/Installer/updater-script" > "$OUT_DIR/META-INF/com/google/android/updater-script"
 cd "$OUT_DIR"
+find . -type f -name "current" -exec rm -f {} +
 find . -exec touch -a -c -m -t 200812311700.00 {} +
 7z a -tzip -mx=5 "WiFi-Fix_v$VERSION-sm7325.zip" .
 cd $SRC
